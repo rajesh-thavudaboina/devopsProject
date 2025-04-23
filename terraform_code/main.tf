dfda360ebfa8bdea37c8e565cd92742fdd03f9bf
rajesh <tsciencerajesh@gmail.com>
2025-04-23 23:02:23 +0530

    added jenkins port

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -73,6 +73,12 @@ resource "aws_security_group" "dpp-sg" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow SSH access from anywhere"
   }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
